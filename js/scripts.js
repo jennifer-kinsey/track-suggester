7b34c2efbfa48457d0cb1531a96d091245906880
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-03-24 10:17:41 -0700

    add specific divs in bootstrap panels. will show panel of track upon submit. images and brief placeholder description added for tracks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("form#track-suggester").submit(function(event){
     event.preventDefault();
+    $("form").hide();
     var number1 = parseInt($("#question1").val());
     var number2 = parseInt($("#question2").val());
     var number3 = parseInt($("#question3").val());
@@ -10,18 +11,17 @@ $(document).ready(function(){
     var total = number1 + number2 + number3 + number4 + number5;
 
     if(total >= 5 && total <= 6) {
-      $("#trackType").text("Ruby/Rails");
+      $("#ruby").show();
     }else if(total >= 7 && total <= 9) {
-      $("#trackType").text("PHP/Drupal");
+      $("#php").show();
     }else if(total >= 10 && total <= 11) {
-      $("#trackType").text("Java/Android");
+      $("#java").show();
     }else if(total >= 12 && total <= 13){
-      $("#trackType").text("CSS/Design");
+      $("#css").show();
     } else {
-      $("#trackType").text("C#/.NET");
+      $("#c-sharp").show();
     }
-    $("#result").show();
-    $("form").hide();
+    $("#refresh").show();
   });
 
   $('#reload').click(function() {
